e499d9038f68cf709070555ed1815c0bf0a591b3
BetaBen1 <bbhardin1@gmail.com>
2017-04-22 12:06:42 -0400

    Question Two
    Question two of the 2015 AP Computer Science Applications Exam was
    completed.

--- 2015FreeResponse/src/question1/DiverseArray.java
+++ 2015FreeResponse/src/question1/DiverseArray.java
@@ -2,6 +2,8 @@ package question1;
 
 public class DiverseArray {
 
+	// Part A //
+	
 	public static int arraySum(int[] array){
 		int sum = 0;
 		for(int i = 0; i<array.length; i++){
@@ -10,6 +12,8 @@ public class DiverseArray {
 		System.out.println(sum);
 		return sum;
 	}
+	
+	// Part B //
 	
 	public static int[] rowSums(int[][] array){
 		
@@ -28,9 +32,20 @@ public class DiverseArray {
 		return rowSums;
 	}
 	
+	// Part C //
+	
 	public static boolean isDiverse(int[][] array){
 		int[] thisArray = rowSums(array);
-		for(int i = 0; ))
+		for(int i = 0; i<thisArray.length; i++){
+			for(int j = i+1; j<thisArray.length; j++){
+				if(thisArray[j] == thisArray[i]){
+					System.out.println("The array is not diverse.");
+					return false;
+				} 
+			}
+		}
+		System.out.println("The array is diverse.");
+		return true;
 		
 	}
 	
